2c7bdf2b331fc13132ae915b0cdf34a4f6307e2d
ZenixR1 <171113912+ZenixR1@users.noreply.github.com>
2026-01-25 20:55:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Z-Test-Electron
+
+This is a template repo for a initially set up electron app.
+
+For cloning, remember you will need to reinitiate npm, install electron, and reconfigure package.json as needed.
+
+See https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app for additional details.
---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
+
+
+const createWindow = () => {
+  const aWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  aWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong');
+    createWindow();
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
